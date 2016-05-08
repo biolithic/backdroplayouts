accfd0217a3d9da6385bb39e17c1418b0f887dd0
Wes Ruvalcaba <wesruv@gmail.com>
2016-05-08 14:42:57 -0400

    Issue #698: Updated Geary layout with preferred markup
    Trying to support more design choices, like color banding across the whole browser width.
    Adding better HTML semantics
    Figured out that full width rows do not need row or col wrappers to lay out correctly.

--- core/layouts/geary/layout--geary.tpl.php
+++ core/layouts/geary/layout--geary.tpl.php
@@ -31,38 +31,36 @@
 
   <?php if ($content['header']): ?>
     <header class="l-header" role="banner" aria-label="<?php print t('Site header'); ?>">
-      <div class="container container-fluid">
-        <div class="row">
-          <?php print $content['header']; ?>
-        </div><!-- /.row -->
+      <div class="l-header-inner container container-fluid">
+        <?php print $content['header']; ?>
       </div><!-- /.container -->
     </header>
   <?php endif; ?>
 
-  <div class="l-wrapper">
-    <div class="l-wrapper-inner container container-fluid">
+  <main class="l-wrapper">
+    <section class="l-wrapper-inner">
 
       <?php if ($messages): ?>
-        <div class="l-messages row" role="status" aria-label="<?php print t('Status messages'); ?>">
-          <div class="col-xs-12">
+        <div class="l-messages" role="status" aria-label="<?php print t('Status messages'); ?>">
+          <div class="l-messages-inner container container-fluid">
             <?php print $messages; ?>
           </div>
         </div>
       <?php endif; ?>
 
-      <div class="l-page-header row">
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?>
-          <div class="col-xs-12">
+      <header class="l-page-header">
+        <div class="l-page-header-inner container container-fluid">
+          <a id="main-content"></a>
+          <?php print render($title_prefix); ?>
+          <?php if ($title): ?>
             <h1 class="title" id="page-title"><?php print $title; ?></h1>
-          </div>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
-      </div>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+        </div>
+      </header>
 
       <?php if ($tabs): ?>
-        <nav class="tabs row" role="tablist" aria-label="<?php print t('Admin Content Navigation Tabs'); ?>">
+        <nav class="tabs container container-fluid" role="tablist" aria-label="<?php print t('Admin Content Navigation Tabs'); ?>">
           <?php print $tabs; ?>
         </nav>
       <?php endif; ?>
@@ -70,43 +68,56 @@
       <?php print $action_links; ?>
 
       <?php if (!empty($content['top'])): ?>
-        <div class="row">
-          <div class="l-top col-md-12">
+        <div class="l-top">
+          <div class="l-top-inner container container-fluid">
             <?php print $content['top']; ?>
           </div>
         </div>
       <?php endif; ?>
 
-      <div class="l-thirds row">
-        <div class="col-md-4">
-          <?php print $content['third1']; ?>
-        </div>
-        <div class="col-md-4">
-          <?php print $content['third2']; ?>
-        </div>
-        <div class="col-md-4">
-          <?php print $content['third3']; ?>
-        </div>
-      </div>
-
-      <?php if (!empty($content['bottom'])): ?>
-        <div class="row">
-          <div class="l-bottom col-md-12">
-            <?php print $content['bottom']; ?>
+      <?php if (!empty($content['third1']) || !empty($content['third2']) || !empty($content['third3'])): ?>
+        <div class="l-thirds">
+          <div class="l-thirds-inner container container-fluid">
+            <div class="l-thirds-inner-2 row">
+              <div class="l-thirds-region col-md-4">
+                <?php print $content['third1']; ?>
+              </div>
+              <div class="l-thirds-region col-md-4">
+                <?php print $content['third2']; ?>
+              </div>
+              <div class="l-thirds-region col-md-4">
+                <?php print $content['third3']; ?>
+              </div>
+            </div>
           </div>
         </div>
       <?php endif; ?>
-    </div><!-- /.l-wrapper-inner /.container -->
 
-    <?php if ($content['footer']): ?>
-      <footer class="l-footer"  role="footer">
-        <div class="container container-fluid">
-          <div class="row">
+      <?php if (!empty($content['bottom'])): ?>
+        <div class="l-bottom">
+          <div class="l-bottom-inner container container-fluid">
+            <div class="l-bottom-inner-2 row">
+              <div class="l-bottom-region col-md-12">
+                <?php print $content['bottom']; ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      <?php endif; ?>
+
+    </section><!-- /.l-wrapper-inner -->
+  </main><!-- /.l-wrapper -->
+
+
+  <?php if ($content['footer']): ?>
+    <footer class="l-footer"  role="footer">
+      <div class="l-footer-inner container container-fluid">
+        <div class="l-footer-inner-2 row">
+          <div class="l-footer-region col-md-12">
             <?php print $content['footer']; ?>
-          </div><!-- /.row -->
-        </div><!-- /.container -->
-      </footer>
-    <?php endif; ?>
-
-  </div><!-- /.l-wrapper -->
+          </div>
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </footer>
+  <?php endif; ?>
 </div><!-- /.geary -->
